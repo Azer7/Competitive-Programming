f54bf9b1c9f5f2597ce2091283c9567af3b60bad
Azer7 <Azer7@users.noreply.github.com>
2018-12-15 20:54:21 -0700

    yea

--- Advent-of-Code/test.cpp
+++ Advent-of-Code/test.cpp
@@ -1,10 +1,14 @@
 // Advent-of-Code.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello World!\n";
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
