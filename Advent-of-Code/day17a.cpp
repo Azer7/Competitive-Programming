b29f2a5327181004b02ab4650205c6c3c794d85c
Azer7 <Azer7@users.noreply.github.com>
2018-12-18 16:21:43 -0700

    initial commit

--- Advent-of-Code/day17a.cpp
+++ Advent-of-Code/day17a.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <iterator>
 #include <sstream>
@@ -10,13 +11,31 @@
 
 #define PI 3.1415926535897932
 
-int main() {
+using namespace std;
+
+void inputFile() {
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable: 4996) //4996 for _CRT_SECURE_NO_WARNINGS equivalent
-	freopen("./input/input.txt", "r", stdin);
+	freopen("../input/day17.txt", "r", stdin);
 #pragma warning(pop)
-#endif
+#endif	
+}
+
+
+vector<vector<char>> map(2000, vector<char>(2000, (char)'.'));
+
+int main() {
+	inputFile();
+	//input data into cin
+	int i = 0;
+	string s;
+	do {
+		getline(cin, s);
+		i++;
+	} while (cin.peek() >= 0);
+
+
 
 
 	return 0;
