b55905dcedbb586d1dfde66428f9616b8bd85b02
Azer7 <Azer7@users.noreply.github.com>
2018-12-18 13:48:35 -0700

    test

---
+++ Advent-of-Code/day17.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <bitset>
+#include <map>
+#include <set>
+#include <math.h>
+
+#define PI 3.1415926535897932
+
+int main() {
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4996) //4996 for _CRT_SECURE_NO_WARNINGS equivalent
+	freopen("./input/input.txt", "r", stdin);
+#pragma warning(pop)
+#endif
+
+
+	return 0;
+}
