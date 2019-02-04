962b2afbd99a98865d9fa69f3d9c27b780e53b76
Azer7 <Azer7@users.noreply.github.com>
2019-02-04 14:02:01 -0700

    test

--- Advent-of-Code/day17a.cpp
+++ Advent-of-Code/day17a.cpp
@@ -22,21 +22,73 @@ void inputFile() {
 #endif	
 }
 
+class Tile
+{
+public:
+	Tile();
+	bool isWall = false;
+	bool isWater = false;
+	bool isDeadEnd = false;
+};
 
-vector<vector<char>> map(2000, vector<char>(2000, (char)'.'));
+Tile::Tile()
+{
+}
+
+vector<vector<Tile>> tMap(2000, vector<Tile>(2000));
+int maximumY = 0;
+int waterCount = 0;
+
+vector<Tile*> deadVec;
+bool dfsDeadEnds(int x, int y) 
+{	//return true if hit a dead end, false if endless
+  	//currently on unknown tile at (x,y) could be water or solid
+  	if (tMap[y][x].isWall || tMap[y][x].isDeadEnd) //check full dead end
+  	{
+  		return true; //dead end = "# or ~"
+ 	} 
+	else if (tMap[y][x].isWater) 
+	{
+		return false; //not dead end but ending
+	}
+
+
+
+}
 
 int main() {
 	inputFile();
 	//input data into cin
-	int i = 0;
-	string s;
 	do {
-		getline(cin, s);
-		i++;
+		string input;
+		bool isX = false;
+
+		getline(cin, input);
+
+		if (input[0] == 'x') {
+			isX = true;
+		}
+		int position, start, end;
+
+		position = stoi(input.substr(2, input.find(",") - 2));
+		start = stoi(input.substr(input.find("=", 4) + 1, input.find("..", 4) - input.find("=", 4) - 1));
+		end = stoi(input.substr(input.find("..", 4) + 2, -1));
+
+		for (int i = start; i <= end; i++) {
+			if (isX) {
+				tMap[i][position].isWall = true;
+				if (i > maximumY)
+					maximumY = i;
+			}
+			else
+			{
+				tMap[position][i].isWall = true;
+				if (position > maximumY)
+					maximumY = position;
+			}
+		}
+
 	} while (cin.peek() >= 0);
-
-
-
-
+	dfsDeadEnds(500, 1);
 	return 0;
 }
---
+++ Kattis/Kattis/test1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <bitset>
+#include <map>
+#include <set>
+#include <math.h>
+
+#define PI 3.1415926535897932
+
+using namespace std;
+
+void inputFile() {
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4996) //4996 for _CRT_SECURE_NO_WARNINGS equivalent
+	freopen("../input/input.txt", "r", stdin);
+#pragma warning(pop)
+#endif
+}
+
+int main() {
+	inputFile();
+	//inputs file
+	
+
+
+	return 0;
+}
